d81e43c74b6f4ebe4624c8502a1bee816833877c
yhs <hastar1332@naver.com>
2026-02-09 21:52:40 +0900

    커밋

--- src/main/java/com/example/lululand/LululandController.java
+++ src/main/java/com/example/lululand/LululandController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
